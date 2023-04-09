4124b0109eaae71db33d35d7894fa6a4a9c12b2c
NinoFranci <nino.franci@student.um.si>
2023-04-10 00:04:07 +0200

    only design for app, zero functionalities

--- app/src/main/java/com/example/projektnodelomilijonar/LoginActivity.kt
+++ app/src/main/java/com/example/projektnodelomilijonar/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.example.projektnodelomilijonar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projektnodelomilijonar.databinding.ActivityLoginBinding
+import com.example.projektnodelomilijonar.databinding.ActivityQuizBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.twLogin.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/projektnodelomilijonar/RegisterActivity.kt
+++ app/src/main/java/com/example/projektnodelomilijonar/RegisterActivity.kt
@@ -9,87 +9,12 @@ import java.sql.*
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-    private var conn: Connection? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getConnection()
-        //executeMySQLQuery()
     }
 
-    private fun getConnection() {
-        val url = "jdbc:mysql://sql303.epizy.com:3306/epiz_33963503_milijonar"
-        val username = "epiz_33963503"
-        val password = "0XYpGgygXZVRG"
-
-        var conn: Connection? = null
-
-        try {
-            // Load the JDBC-ODBC bridge driver
-            Class.forName("com.mysql.cj.jdbc.Driver")
-
-            // Establish the database connection
-            conn = DriverManager.getConnection(url, username, password)
-
-            // Print a message to indicate success
-            binding.textView3.text = "Connected to the database."
-
-            // Perform database operations here...
-            // For example:
-            // val stmt = conn.createStatement()
-            // val rs = stmt.executeQuery("SELECT * FROM mytable")
-            // while (rs.next()) {
-            //     println(rs.getString("column_name"))
-            // }
-
-        } catch (ex: ClassNotFoundException) {
-            // Handle driver loading errors
-            binding.textView3.text = "Error: could not load JDBC-ODBC bridge driver"
-            ex.printStackTrace()
-        } catch (ex: SQLException) {
-            // Handle database connection errors
-            binding.textView3.text = "Error: could not connect to the database"
-            ex.printStackTrace()
-        } finally {
-            // Close the database connection (if it was established)
-            if (conn != null) {
-                try {
-                    conn.close()
-                    println("Disconnected from the database.")
-                } catch (ex: SQLException) {
-                    println("Error: could not disconnect from the database")
-                    ex.printStackTrace()
-                }
-            }
-        }
-    }
-
-    private fun executeMySQLQuery() {
-        var stmt: Statement? = null
-        var resultset: ResultSet? = null
-
-        try {
-            stmt = conn!!.createStatement()
-            resultset = stmt!!.executeQuery("SELECT * FROM my_table")
-
-            while (resultset.next()) {
-                val id = resultset.getInt("id")
-                val name = resultset.getString("name")
-                val age = resultset.getInt("age")
-
-                println("id: $id, name: $name, age: $age")
-            }
-        } catch (ex: SQLException) {
-            // handle any errors
-            ex.printStackTrace()
-        } finally {
-            // release resources
-            resultset?.close()
-            stmt?.close()
-            conn?.close()
-        }
-    }
 }
